7d355f013d0c0678ed271d918639a1438a7ff343
Anjali Rachel Benjamin <an653262@dal.ca>
2024-07-12 10:23:18 -0300

    added files

--- Tutorial 5/my-node-app/functions/users.js
+++ Tutorial 5/my-node-app/functions/users.js
@@ -1,133 +1,100 @@
-// functions/users.js
-
+// Import required modules
 const { v4: uuidv4 } = require('uuid');
 
+// Data store for users (mocked database)
 let users = [
   { email: 'abc@abc.ca', firstName: 'ABC', id: '5abf6783' },
   { email: 'xyz@xyz.ca', firstName: 'XYZ', id: '5abf674563' }
 ];
 
+// GET all users
 exports.handler = async (event, context) => {
-  switch (event.httpMethod) {
-    case 'GET':
-      if (event.path === '/users') {
-        return getUsers();
-      } else if (event.path.startsWith('/user/')) {
-        return getUserById(event);
-      } else {
-        return notFound();
-      }
-      break;
-    case 'POST':
-      if (event.path === '/users') {
-        return addUser(event);
-      } else {
-        return notAllowed();
-      }
-      break;
-    case 'PUT':
-      if (event.path.startsWith('/user/')) {
-        return updateUser(event);
-      } else {
-        return notAllowed();
-      }
-      break;
-    default:
-      return notAllowed();
-  }
-};
-
-function getUsers() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Users retrieved',
-      success: true,
-      users: users
-    })
-  };
-}
-
-function getUserById(event) {
-  const userId = event.path.split('/')[2];
-  const user = users.find(u => u.id === userId);
-  if (user) {
+  if (event.httpMethod === 'GET' && event.path === '/.netlify/functions/users') {
     return {
       statusCode: 200,
       body: JSON.stringify({
+        message: 'Users retrieved',
         success: true,
-        user: user
-      })
-    };
-  } else {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({
-        success: false,
-        message: 'User not found'
+        users: users
       })
     };
   }
-}
 
-function addUser(event) {
-  const { email, firstName } = JSON.parse(event.body);
-  const newUser = {
-    email: email,
-    firstName: firstName,
-    id: uuidv4()
-  };
-  users.push(newUser);
-  return {
-    statusCode: 201,
-    body: JSON.stringify({
-      message: 'User added',
-      success: true
-    })
-  };
-}
+  // GET user by ID
+  else if (event.httpMethod === 'GET' && event.path.startsWith('/.netlify/functions/user/')) {
+    const userId = event.path.split('/')[3];
+    const user = users.find(u => u.id === userId);
+    if (user) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          success: true,
+          user: user
+        })
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          success: false,
+          message: 'User not found'
+        })
+      };
+    }
+  }
 
-function updateUser(event) {
-  const userId = event.path.split('/')[2];
-  const { email, firstName } = JSON.parse(event.body);
-  const userIndex = users.findIndex(u => u.id === userId);
-  if (userIndex !== -1) {
-    users[userIndex].email = email || users[userIndex].email;
-    users[userIndex].firstName = firstName || users[userIndex].firstName;
+  // POST add user
+  else if (event.httpMethod === 'POST' && event.path === '/.netlify/functions/users') {
+    const { email, firstName } = JSON.parse(event.body);
+    const newUser = {
+      email: email,
+      firstName: firstName,
+      id: uuidv4()
+    };
+    users.push(newUser);
     return {
-      statusCode: 200,
+      statusCode: 201,
       body: JSON.stringify({
-        message: 'User updated',
+        message: 'User added',
         success: true
       })
     };
-  } else {
+  }
+
+  // PUT update user
+  else if (event.httpMethod === 'PUT' && event.path.startsWith('/.netlify/functions/user/')) {
+    const userId = event.path.split('/')[3];
+    const { email, firstName } = JSON.parse(event.body);
+    const userIndex = users.findIndex(u => u.id === userId);
+    if (userIndex !== -1) {
+      users[userIndex].email = email || users[userIndex].email;
+      users[userIndex].firstName = firstName || users[userIndex].firstName;
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'User updated',
+          success: true
+        })
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          success: false,
+          message: 'User not found'
+        })
+      };
+    }
+  }
+
+  // Handle other requests
+  else {
     return {
       statusCode: 404,
       body: JSON.stringify({
         success: false,
-        message: 'User not found'
+        message: 'Endpoint not found'
       })
     };
   }
-}
-
-function notFound() {
-  return {
-    statusCode: 404,
-    body: JSON.stringify({
-      success: false,
-      message: 'Endpoint not found'
-    })
-  };
-}
-
-function notAllowed() {
-  return {
-    statusCode: 405,
-    body: JSON.stringify({
-      success: false,
-      message: 'Method not allowed'
-    })
-  };
-}
+};
