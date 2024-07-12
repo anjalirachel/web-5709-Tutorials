a087080cc693b0e72f715fe77b07733a0a43440c
Anjali Rachel Benjamin <an653262@dal.ca>
2024-07-12 10:01:04 -0300

    added files

--- Tutorial 5/my-node-app/functions/users.js
+++ Tutorial 5/my-node-app/functions/users.js
@@ -1,12 +1,4 @@
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
 
 let users = [
   { email: 'abc@abc.ca', firstName: 'ABC', id: '5abf6783' },
@@ -14,68 +6,127 @@ let users = [
 ];
 
 // GET all users
-app.get('/users', (req, res) => {
-  res.status(200).json({
-    message: 'Users retrieved',
-    success: true,
-    users: users
-  });
-});
+exports.handler = async (event, context) => {
+  if (event.httpMethod === 'GET') {
+    // GET /users endpoint
+    if (event.path === '/users') {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Users retrieved',
+          success: true,
+          users: users
+        })
+      };
+    }
 
-// GET user by ID
-app.get('/user/:id', (req, res) => {
-  const user = users.find(u => u.id === req.params.id);
-  if (user) {
-    res.status(200).json({
-      success: true,
-      user: user
-    });
-  } else {
-    res.status(404).json({
-      success: false,
-      message: 'User not found'
-    });
+    // GET /user/:id endpoint
+    else if (event.path.startsWith('/user/')) {
+      const userId = event.path.split('/')[2];
+      const user = users.find(u => u.id === userId);
+      if (user) {
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            success: true,
+            user: user
+          })
+        };
+      } else {
+        return {
+          statusCode: 404,
+          body: JSON.stringify({
+            success: false,
+            message: 'User not found'
+          })
+        };
+      }
+    }
+
+    // Handle other GET requests
+    else {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          success: false,
+          message: 'Endpoint not found'
+        })
+      };
+    }
   }
-});
+
+  // Handle other HTTP methods
+  else {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({
+        success: false,
+        message: 'Method not allowed'
+      })
+    };
+  }
+};
 
 // POST add user
-app.post('/add', (req, res) => {
-  const newUser = {
-    email: req.body.email,
-    firstName: req.body.firstName,
-    id: uuidv4() // Generate a unique ID for the new user
-  };
-  users.push(newUser);
-  res.status(201).json({
-    message: 'User added',
-    success: true
-  });
-});
+exports.add = async (event, context) => {
+  if (event.httpMethod === 'POST') {
+    const { email, firstName } = JSON.parse(event.body);
+    const newUser = {
+      email: email,
+      firstName: firstName,
+      id: uuidv4()
+    };
+    users.push(newUser);
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: 'User added',
+        success: true
+      })
+    };
+  } else {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({
+        success: false,
+        message: 'Method not allowed'
+      })
+    };
+  }
+};
 
 // PUT update user
-app.put('/update/:id', (req, res) => {
-  const userIndex = users.findIndex(u => u.id === req.params.id);
-  if (userIndex !== -1) {
-    users[userIndex].email = req.body.email || users[userIndex].email;
-    users[userIndex].firstName = req.body.firstName || users[userIndex].firstName;
-    res.status(200).json({
-      message: 'User updated',
-      success: true
-    });
+exports.update = async (event, context) => {
+  if (event.httpMethod === 'PUT') {
+    const userId = event.path.split('/')[2];
+    const { email, firstName } = JSON.parse(event.body);
+    const userIndex = users.findIndex(u => u.id === userId);
+    if (userIndex !== -1) {
+      users[userIndex].email = email || users[userIndex].email;
+      users[userIndex].firstName = firstName || users[userIndex].firstName;
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'User updated',
+          success: true
+        })
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          success: false,
+          message: 'User not found'
+        })
+      };
+    }
   } else {
-    res.status(404).json({
-      success: false,
-      message: 'User not found'
-    });
+    return {
+      statusCode: 405,
+      body: JSON.stringify({
+        success: false,
+        message: 'Method not allowed'
+      })
+    };
   }
-});
-
-// Create a basic HTTP server
-const server = http.createServer(app);
-
-// Define the port and start the server
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-module.exports = app;
+};
