f5f54911650ec8eb6d7ac93496187c9115e95f5b
Anjali Rachel Benjamin <an653262@dal.ca>
2024-07-12 10:17:38 -0300

    added files

--- Tutorial 5/my-node-app/functions/users.js
+++ Tutorial 5/my-node-app/functions/users.js
@@ -1,3 +1,5 @@
+// functions/users.js
+
 const { v4: uuidv4 } = require('uuid');
 
 let users = [
@@ -5,128 +7,127 @@ let users = [
   { email: 'xyz@xyz.ca', firstName: 'XYZ', id: '5abf674563' }
 ];
 
-// GET all users
 exports.handler = async (event, context) => {
-  if (event.httpMethod === 'GET') {
-    // GET /users endpoint
-    if (event.path === '/users') {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: 'Users retrieved',
-          success: true,
-          users: users
-        })
-      };
-    }
-
-    // GET /user/:id endpoint
-    else if (event.path.startsWith('/user/')) {
-      const userId = event.path.split('/')[2];
-      const user = users.find(u => u.id === userId);
-      if (user) {
-        return {
-          statusCode: 200,
-          body: JSON.stringify({
-            success: true,
-            user: user
-          })
-        };
+  switch (event.httpMethod) {
+    case 'GET':
+      if (event.path === '/users') {
+        return getUsers();
+      } else if (event.path.startsWith('/user/')) {
+        return getUserById(event);
       } else {
-        return {
-          statusCode: 404,
-          body: JSON.stringify({
-            success: false,
-            message: 'User not found'
-          })
-        };
+        return notFound();
       }
-    }
-
-    // Handle other GET requests
-    else {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({
-          success: false,
-          message: 'Endpoint not found'
-        })
-      };
-    }
-  }
-
-  // Handle other HTTP methods
-  else {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({
-        success: false,
-        message: 'Method not allowed'
-      })
-    };
+      break;
+    case 'POST':
+      if (event.path === '/users') {
+        return addUser(event);
+      } else {
+        return notAllowed();
+      }
+      break;
+    case 'PUT':
+      if (event.path.startsWith('/user/')) {
+        return updateUser(event);
+      } else {
+        return notAllowed();
+      }
+      break;
+    default:
+      return notAllowed();
   }
 };
 
-// POST add user
-exports.add = async (event, context) => {
-  if (event.httpMethod === 'POST') {
-    const { email, firstName } = JSON.parse(event.body);
-    const newUser = {
-      email: email,
-      firstName: firstName,
-      id: uuidv4()
-    };
-    users.push(newUser);
+function getUsers() {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Users retrieved',
+      success: true,
+      users: users
+    })
+  };
+}
+
+function getUserById(event) {
+  const userId = event.path.split('/')[2];
+  const user = users.find(u => u.id === userId);
+  if (user) {
     return {
-      statusCode: 201,
+      statusCode: 200,
       body: JSON.stringify({
-        message: 'User added',
+        success: true,
+        user: user
+      })
+    };
+  } else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        success: false,
+        message: 'User not found'
+      })
+    };
+  }
+}
+
+function addUser(event) {
+  const { email, firstName } = JSON.parse(event.body);
+  const newUser = {
+    email: email,
+    firstName: firstName,
+    id: uuidv4()
+  };
+  users.push(newUser);
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: 'User added',
+      success: true
+    })
+  };
+}
+
+function updateUser(event) {
+  const userId = event.path.split('/')[2];
+  const { email, firstName } = JSON.parse(event.body);
+  const userIndex = users.findIndex(u => u.id === userId);
+  if (userIndex !== -1) {
+    users[userIndex].email = email || users[userIndex].email;
+    users[userIndex].firstName = firstName || users[userIndex].firstName;
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'User updated',
         success: true
       })
     };
   } else {
     return {
-      statusCode: 405,
+      statusCode: 404,
       body: JSON.stringify({
         success: false,
-        message: 'Method not allowed'
+        message: 'User not found'
       })
     };
   }
-};
+}
 
-// PUT update user
-exports.update = async (event, context) => {
-  if (event.httpMethod === 'PUT') {
-    const userId = event.path.split('/')[2];
-    const { email, firstName } = JSON.parse(event.body);
-    const userIndex = users.findIndex(u => u.id === userId);
-    if (userIndex !== -1) {
-      users[userIndex].email = email || users[userIndex].email;
-      users[userIndex].firstName = firstName || users[userIndex].firstName;
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: 'User updated',
-          success: true
-        })
-      };
-    } else {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({
-          success: false,
-          message: 'User not found'
-        })
-      };
-    }
-  } else {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({
-        success: false,
-        message: 'Method not allowed'
-      })
-    };
-  }
-};
+function notFound() {
+  return {
+    statusCode: 404,
+    body: JSON.stringify({
+      success: false,
+      message: 'Endpoint not found'
+    })
+  };
+}
+
+function notAllowed() {
+  return {
+    statusCode: 405,
+    body: JSON.stringify({
+      success: false,
+      message: 'Method not allowed'
+    })
+  };
+}
